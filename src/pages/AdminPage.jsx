a41776d103f663c72d5471d7e6617e6aedd0810c
devofathurisqi <114217942+devofathurisqi@users.noreply.github.com>
2026-07-29 16:20:58 +0700

    Update AdminPage.jsx

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -21,8 +21,8 @@ export default function AdminPage() {
   const [popupAdmins, setPopupAdmins] = useState([
     { name: 'CS Putri', number: '0858-8245-0652' },
     { name: 'CS Renita', number: '0853-3909-5119' },
-    { name: 'CS Alya', number: '0821-1329-4501' },
-    { name: 'Vumin - AI Bot assistant', number: '0856-9406-0878' }
+    { name: 'CS Alya', number: '0821-1329-4501' }
+    // { name: 'Vumin - AI Bot assistant', number: '0856-9406-0878' }
   ]);
   const [popupBankAccounts, setPopupBankAccounts] = useState([
     { bankName: 'BCA', accountNumber: '167-160453-4', holderName: 'Pramesthy Kaulaswara Annur' },
